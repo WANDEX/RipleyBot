1acd379334e52a340b1de56816157e9c0dc1e6a5
WANDEX <downloadfils@gmail.com>
2018-10-16 21:10:20 +0300

    getting token & proxy from file

--- src/bot.py
+++ src/bot.py
@@ -9,16 +9,13 @@ import telebot
 from telebot.types import Message
 from telebot import apihelper
 
-file = open('src/token&proxy.txt', 'r')
+file = open('temp/token&proxy.txt', 'r')
 
-TOKEN = [line for line in file if "BOT_TOKEN" in line]
-PROXY = [line for line in file if "BOT_PROXY" in line]
+TOKEN = file.readline()[10:-1]  # BOT_TOKEN=
+PROXY = file.readline()[10:-1]  # BOT_PROXY=
+print("\nTOKEN:{0}\nPROXY:{1}".format(TOKEN, PROXY))
 
-# TOKEN = open('BOT_TOKEN')
-# PROXY = os.environ.get('BOT_PROXY')
-print(str(PROXY) + '\n' + str(TOKEN) + '\n')
 apihelper.proxy = {'https': PROXY}
-
 bot = telebot.TeleBot(TOKEN)
 
 
@@ -32,4 +29,4 @@ def upper(message: Message):
     bot.reply_to(message, message.text.upper())
 
 
-# bot.polling()
+bot.polling()
